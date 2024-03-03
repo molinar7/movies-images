0859f09c8963d228d364a2e0e33500419fc3dd17
Mario <molinarmario3@gmail.com>
2024-03-02 21:32:05 -0600

    Adding swagger

--- src/main/java/com/itimindree/movie/movieimagesapi/controller/MovieImageController.java
+++ src/main/java/com/itimindree/movie/movieimagesapi/controller/MovieImageController.java
@@ -4,6 +4,8 @@ import com.itimindree.movie.movieimagesapi.model.MovieImage;
 import com.itimindree.movie.movieimagesapi.service.MovieImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+
 
 import java.util.List;
 import java.util.Map;
@@ -15,21 +17,25 @@ public class MovieImageController {
     @Autowired
     private MovieImageService movieImageService;
 
+    @Operation(summary = "List all movie images grouped by release year")
     @GetMapping("/all/grouped-by-year")
     public Map<Integer,List<MovieImage>> getAllMovieImagesByReleaseYear() {
         return movieImageService.listAllMovieImagesGroupedByReleaseYear();
     }
 
+    @Operation(summary = "List all movie images by year")
     @GetMapping("/year/{year}")
     public List<MovieImage> getMovieImagesByYear(@PathVariable int year) {
         return movieImageService.listMovieImagesByReleaseYear(year);
     }
 
+    @Operation(summary = "Vote movie image")
     @PostMapping("/vote/{id}")
     public MovieImage voteMovieImage(@PathVariable Long id, @RequestParam boolean upVote) {
         return movieImageService.voteMovieImage(id, upVote);
     }
 
+    @Operation(summary = "Get details of a movie image")
     @GetMapping("/{id}")
     public MovieImage getMovieImageDetails(@PathVariable Long id) {
         return movieImageService.getMovieImageDetails(id);
